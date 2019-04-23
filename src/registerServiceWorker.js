b8046515816d7fc5b1ea0a2280020519c6e27c96
Ronan <rsf.sho@gmail.com>
2019-04-23 14:35:59 -0500

    fix sw loading

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -35,6 +35,6 @@ if (process.env.NODE_ENV === 'production') {
 if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('sw.js');
   });
 }
