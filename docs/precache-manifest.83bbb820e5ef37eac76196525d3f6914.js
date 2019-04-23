ae7c74739a7ebfe85ef9b564aba1a2de29d00791
Ronan <rsf.sho@gmail.com>
2019-04-23 08:55:00 -0500

    v1

---
+++ docs/precache-manifest.83bbb820e5ef37eac76196525d3f6914.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "257a74dd47df575ed848",
+    "url": "css/app.39e94287.css"
+  },
+  {
+    "revision": "257a74dd47df575ed848",
+    "url": "js/app.98c64845.js"
+  },
+  {
+    "revision": "ca9bbbc96fb204f4e4dc",
+    "url": "css/chunk-vendors.de600c15.css"
+  },
+  {
+    "revision": "ca9bbbc96fb204f4e4dc",
+    "url": "js/chunk-vendors.e13f216f.js"
+  },
+  {
+    "revision": "037fdb771ad10009cc51b7be3759fee6",
+    "url": "img/done.037fdb77.svg"
+  },
+  {
+    "revision": "72c4aeb52403bcc16317af9216e5f33b",
+    "url": "img/sign.72c4aeb5.svg"
+  },
+  {
+    "revision": "82ddb21822479bb6543dcf1bec1b6d1e",
+    "url": "img/logo.82ddb218.svg"
+  },
+  {
+    "revision": "8cfff9eecb263fa8ee94c711f76100ff",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
